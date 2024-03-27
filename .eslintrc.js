3c9606aab3076c0812c975dd6c378775f6c7f846
Stephanie <stephaniiehallberg@gmail.com>
2024-03-27 10:19:20 +0100

    fix: eslint with reac-in-scope & endOfLine auto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+      ,
+    ],
+  },
 };
